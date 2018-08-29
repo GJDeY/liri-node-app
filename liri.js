1658c9c808b00d449b66ccc5510ae6a8ab48d12c
GJDeY <gregoryjdeyoung@u.northwestern.edu>
2018-08-29 17:43:59 -0500

    added/required moment package in liri.js

--- liri.js
+++ liri.js
@@ -1,7 +1,12 @@
 require("dotenv").config();
-var keys = require("./keys.js");
-console.log(keys);
+//require the filesystem
+var fs = require('fs');
 
+var keys = require("./keys.js");
+//console.log(keys);
+
+//linking the client Id and keys from .env
+var spotify = new Spotify(keys.spotify);
 
 //require request pkg 
 var request = require('request');
@@ -11,9 +16,16 @@ request('http://www.google.com', function (error, response, body) {
     console.log('body:', body); // Print the HTML for the Google homepage.
 });
 
+
+//require moment
+var moment = require('moment');
+moment().format();
+
+
+var music = process.argv[2];
+console.log("music search");
 //add in command line arguments - process.argv([2])
 //                              - process.argv([3])
-
+var musicSearch = "spotify-this-song" + process.argv[2];
 //do if else function here
-
-//
+console.log(musicSearch);
