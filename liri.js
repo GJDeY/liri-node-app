9ce8b557275c4448377c4265ea936110f03cb48a
GJDeY <gregoryjdeyoung@u.northwestern.edu>
2018-08-29 09:31:24 -0500

    required request pkg

--- liri.js
+++ liri.js
@@ -1,4 +1,19 @@
 require("dotenv").config();
-var keys = require("./keys");
+var keys = require("./keys");//possibly "./keys.js"???
 console.log(keys);
+
+
+//require request pkg 
+var request = require('request');
+request('http://www.google.com', function (error, response, body) {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+});
+
+//add in command line arguments - process.argv([2])
+//                              - process.argv([3])
+
 //do if else function here
+
+//
