dae419fc264187450f646499e3de59a79ac012cf
GJDeY <gregoryjdeyoung@u.northwestern.edu>
2018-08-29 23:55:56 -0500

    all day effort

--- keys.js
+++ keys.js
@@ -1,7 +1,23 @@
+
+//to show keys.js & liri.js files are linked when making queries in the terminal
 console.log('this is loaded');
 
+
+//API keys for spotify
 exports.spotify = {
     id: process.env.SPOTIFY_ID,
     secret: process.env.SPOTIFY_SECRET
 };
 
+
+//omdb API KEY
+
+//exports.omdb = {
+//   Key: process.env.omdbAPIKey
+//};
+
+
+//app_id for bands in town
+//exports.bandsInTown = {
+//   app_id: process.env.bandsInTown
+//};
--- liri.js
+++ liri.js
@@ -5,8 +5,7 @@ var fs = require('fs');
 var keys = require("./keys.js");
 //console.log(keys);
 
-//linking the client Id and keys from .env
-var spotify = new Spotify(keys.spotify);
+
 
 //require request pkg 
 var request = require('request');
@@ -17,15 +16,89 @@ request('http://www.google.com', function (error, response, body) {
 });
 
 
+
+
 //require moment
 var moment = require('moment');
 moment().format();
 
 
-var music = process.argv[2];
+
+
+search = process.argv[2];
+var search = 'concert-this';
+switch (search) {
+    case 'spotify-this-song':
+        console.log('is what you searched for.');
+        break;
+    case 'movie-this':
+        console.log('is what you searched for.');
+        break;
+    case 'concert-this':
+        console.log('is in town!');
+        break;
+    default:
+        console.log('sorry, no information for ' + search + '!');
+}
+
+
+///////////  OMDB Movie search section  ////////////
+var omdbSearch = process.argv[3];
+
+
+//          requesting to OMDB API
+var queryUrl = "http://www.omdbapi.com/?t=" + omdbSearch + "&y=&plot=short&apikey=*******";
+//////////fix above *'s with key
+
+//  show what you searched for (query URL to the OMDB API.)
+console.log(queryUrl);
+
+request(queryUrl, function (error, response, body) {
+
+    // If the request is successful
+    if (!error && response.statusCode === 200) {
+
+    }
+});
+console.log("movie-this");
+
+
+
+
+
+//////////////Spotify-this-song section
+var Spotify = require('node-spotify-api');
+
+//linking the client Id and keys from .env
+var music = new Spotify(keys.spotify);
+
+
+
+
+
+var spotify = process.argv[3];
 console.log("music search");
-//add in command line arguments - process.argv([2])
-//                              - process.argv([3])
-var musicSearch = "spotify-this-song" + process.argv[2];
+//add in command line arguments music- process.argv([2])
+//                              "spotify-this-song' = process.argv([3])
+//var spotify = "spotify-this-song" + process.argv[2];
 //do if else function here
-console.log(musicSearch);
+//console.log(spotify);
+
+
+//spotify-this-song if else statement
+//if (spotify === true) {
+//}
+//else if (spotify === false) {
+//}
+//else {
+//run liri.js
+//  liri.js
+//}
+
+
+
+
+
+
+
+//append commands to log.txt file
